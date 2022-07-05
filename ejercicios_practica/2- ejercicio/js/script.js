4e412b1e9adfbfb3a87f59116b8bca18bea90c88
DardoRL <69401941+DardoRL@users.noreply.github.com>
2022-07-05 16:04:32 -0300

    Add files via upload

--- ejercicios_practica/2- ejercicio/js/script.js
+++ ejercicios_practica/2- ejercicio/js/script.js
@@ -17,3 +17,23 @@ Obtener todos los botones utilizando la clase "btn",
 luego a todos los botones agregar la clase "agua"
 
 */
+
+/* Enunciado 1 */
+
+let btn1 = document.querySelector("#boton1");
+let btn2 = document.querySelector("#boton2");
+let btn3 = document.querySelector("#boton3");
+btn1.textContent = "Quienes Somos";
+btn2.textContent = "Productos";
+btn3.textContent = "Contactenos";
+
+/* Enunciado 2 */
+
+const classBtn = document.getElementsByClassName("btn")[0];
+classBtn.classList.add("agua");
+
+
+/* console.log (classBtn.classList); */
+
+
+
