3e43c6fdbea9a540089dd52ae1ac0ce8c8ca5d4e
DardoRL <69401941+DardoRL@users.noreply.github.com>
2022-07-05 16:03:34 -0300

    Add files via upload

--- ejercicios_practica/1- ejercicio/js/script.js
+++ ejercicios_practica/1- ejercicio/js/script.js
@@ -14,3 +14,8 @@ Leer el título del "header" del post e imprimirlo en consola.
 Cambiar título utilizando javascript por otro cualquiera.
 
 */
+const TITULOH2 = document.querySelector("#myHeader");
+console.log (TITULOH2.textContent);
+TITULOH2.textContent= "Este es el nuevo titulo de H2";
+console.log (TITULOH2.textContent);
+
